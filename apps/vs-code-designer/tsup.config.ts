92dcea6210ccce8c1e1b810d3661812a410b3eb4
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-05-10 16:24:43 -0700

    Add initial commit

--- apps/vs-code-designer/tsup.config.ts
+++ apps/vs-code-designer/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/main.ts'],
+  entry: ['src/main.ts', 'src/e2e-tests/*.test.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
