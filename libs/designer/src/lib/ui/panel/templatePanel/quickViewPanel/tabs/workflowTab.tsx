92c973a6fce06fc65f15eb3f09d1e60bd4c15f7b
Elaina Lee <hyehwalee@microsoft.com>
2024-07-16 11:44:37 -0700

    changed back to working version

--- libs/designer/src/lib/ui/panel/templatePanel/quickViewPanel/tabs/workflowTab.tsx
+++ libs/designer/src/lib/ui/panel/templatePanel/quickViewPanel/tabs/workflowTab.tsx
@@ -2,10 +2,7 @@ import type { AppDispatch, RootState } from '../../../../../core/state/templates
 import { useSelector } from 'react-redux';
 import type { IntlShape } from 'react-intl';
 import constants from '../../../../../common/constants';
-import {
-  useTheme,
-  // , Image
-} from '@fluentui/react';
+import { useTheme, Image } from '@fluentui/react';
 import { useMemo } from 'react';
 import type { TemplatePanelTab } from '@microsoft/designer-ui';
 import { closePanel, openCreateWorkflowPanelView } from '../../../../../core/state/templates/panelSlice';
@@ -18,8 +15,7 @@ export const WorkflowPanel: React.FC = () => {
 
   return imageName ? (
     <div className="msla-template-workflow-preview">
-      {/* <Image src={imageName} alt={manifest?.title} /> */}
-      <img src={imageName} alt={manifest?.title} />
+      <Image src={imageName} alt={manifest?.title} />
     </div>
   ) : null;
 };
